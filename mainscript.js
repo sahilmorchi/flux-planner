2d103f01691b3f49b225fedf59fb107a7a74bc16
sahilmorchi <sahil.morchi@gmail.com>
2017-07-03 23:37:57 -0700

    Added page header changes and progress circle implementation

--- mainscript.js
+++ mainscript.js
@@ -7,3 +7,45 @@ $('.fa-bars').click(function(){
         });
     },100);
 });
+
+$('#dashboard').click(function(){
+    $('.pageTitle').html('Dashboard');
+});
+
+$('#testScores').click(function(){
+    $('.pageTitle').html('Test Scores');
+    $('#circle').circleProgress({
+	value: calcPercentageSAT(1500),
+	size: 100,
+    startAngle: (Math.PI)*3/2,
+	fill: {
+		gradient: ["red", "orange"]
+	}
+});
+});
+
+$('#calendar').click(function(){
+    $('.pageTitle').html('Calendar');
+});
+
+$('#analytics').click(function(){
+    $('.pageTitle').html('Analytics');
+});
+
+
+
+
+
+/*
+$('#circle').circleProgress({
+    size: 100,
+    startAngle: (Math.PI)*3/2,
+    value: calcPercentageSAT(1500)
+  }).on('circle-animation-progress', function(event, progress) {
+    $(this).find('strong').html(Math.round(100 * progress) + '<i>%</i>');
+  });
+*/
+
+function calcPercentageSAT(score){
+    return score/1600;
+}
